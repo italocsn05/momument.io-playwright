91e57d33a3fc62c6f6e2feac23adf82f2fcc0695
Italo Cabral <italocsn05@gmail.com>
2025-02-19 11:10:11 -0300

    chore: reduce timeout values in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,13 +7,15 @@ export default defineConfig({
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
-    timeout: 300000,
+    timeout: 30000,
     use: {
         trace: 'on-first-retry',
         baseURL: process.env.BASE_URL,
         testIdAttribute: 'data-testid',
     },
-
+    expect: {
+        timeout: 10000,
+    },
     projects: [
         {
             name: 'chromium',
